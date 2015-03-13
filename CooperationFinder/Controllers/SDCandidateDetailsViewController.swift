d8639476e3ba81333eae88ceb9aa42aab97d3827
Rafal Kwiatkowski <rafal.m.kwiatkowski@gmail.com>
2015-03-13 20:15:05 +0100

    Added contacting with approved applicants

--- CooperationFinder/Controllers/SDCandidateDetailsViewController.swift
+++ CooperationFinder/Controllers/SDCandidateDetailsViewController.swift
@@ -12,6 +12,7 @@ class SDCandidateDetailsViewController: UIViewController {
     @IBOutlet weak var userFullName: UILabel?
     @IBOutlet weak var approveButton: UIButton?
     @IBOutlet weak var denyButton: UIButton?
+    @IBOutlet weak var contactButton: UIButton?
     @IBOutlet weak var descriptionView: UILabel?
     @IBOutlet var titleTop: NSLayoutConstraint!
     @IBOutlet var tagCloudView : TagCloudView!
@@ -21,26 +22,38 @@ class SDCandidateDetailsViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        var a : UIView?
-        
-        self.tagCloudView.tags = self.application?.user?.tags
-        
-        if (self.application?.status == ApplicationStatusApproved || self.application?.status == ApplicationStatusRejected) {
-            self.approveButton?.hidden = true
-            self.denyButton?.hidden = true
-            self.titleTop.constant = 16.0
-        }
+        self.reloadData()
     }
     
     override func viewWillAppear(animated: Bool) {
         super.viewWillAppear(animated)
-        userFullName?.text = application?.user?.nameAndCompany()
-        descriptionView?.text = application?.user?.desc
     }
     
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
+    }
+    
+    func reloadData() {
+        userFullName?.text = application?.user?.nameAndCompany()
+        descriptionView?.text = application?.user?.desc
+        self.tagCloudView.tags = self.application?.user?.tags
+        
+        if (self.application?.status == ApplicationStatusRejected) {
+            self.approveButton?.hidden = true
+            self.denyButton?.hidden = true
+            self.contactButton?.hidden = true
+            self.titleTop.constant = 16.0
+        } else if (self.application?.status == ApplicationStatusApproved) {
+            self.approveButton?.hidden = true
+            self.denyButton?.hidden = true
+            self.contactButton?.hidden = false
+            self.titleTop.constant = 56.0
+        } else {
+            self.approveButton?.hidden = false
+            self.denyButton?.hidden = false
+            self.contactButton?.hidden = true
+            self.titleTop.constant = 56.0
+        }
     }
     
     @IBAction func approveButtonTapped(sender: UIButton) {
@@ -50,7 +63,7 @@ class SDCandidateDetailsViewController: UIViewController {
         approveButton?.addActivityIndicator()
         Application.saveApplication(application!, completion: { (success: Bool) -> Void in
             if success == true {
-                self.performSegueWithIdentifier("UnwindSegue", sender: self)
+                self.reloadData()
             } else {
                 let alertView = UIAlertView(title: NSLocalizedString("Oops", comment: ""), message: NSLocalizedString("Failed to save the application", comment: ""), delegate: nil, cancelButtonTitle: "OK")
                 alertView.show()
@@ -66,7 +79,7 @@ class SDCandidateDetailsViewController: UIViewController {
         denyButton?.addActivityIndicator()
         Application.saveApplication(application!, completion: { (success: Bool) -> Void in
             if (success) {
-                self.performSegueWithIdentifier("UnwindSegue", sender: self)
+                self.reloadData()
             } else {
                 let alertView = UIAlertView(title: NSLocalizedString("Oops", comment: ""), message: NSLocalizedString("Failed to save the application", comment: ""), delegate: nil, cancelButtonTitle: "OK")
                 alertView.show()
@@ -74,4 +87,16 @@ class SDCandidateDetailsViewController: UIViewController {
             self.denyButton?.removeActivityIndicator()
         })
     }
+    
+    @IBAction func contactButtonTapped(sender: AnyObject) {
+        
+        if let email = self.application?.user?.email {
+            var urlString = "mailto:\(email)"
+            if let url = NSURL(string: urlString) {
+                UIApplication.sharedApplication().openURL(url)
+            }
+
+        }
+    }
+    
 }
